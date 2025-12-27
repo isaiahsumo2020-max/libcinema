9fa8a2c7a90e9fa696cc83adec4331e3d4b0e59b
isaiahsumo2020-max <isaiahsumo2020@gmail.com>
2025-12-27 19:48:28 +0000

    Fix WatchList import path

--- src/services/WatchList.js
+++ src/services/WatchList.js
@@ -1,16 +1,16 @@
-export function getWatchlist() {
+export function getWatchList() {
   return JSON.parse(localStorage.getItem('watchlist')) || []
 }
 
-export function addToWatchlist(movie) {
-  const list = getWatchlist()
+export function addToWatchList(movie) {
+  const list = getWatchList()
   if (!list.find(m => m.id === movie.id)) {
     list.push(movie)
     localStorage.setItem('watchlist', JSON.stringify(list))
   }
 }
 
-export function removeFromWatchlist(id) {
-  const list = getWatchlist().filter(m => m.id !== id)
+export function removeFromWatchList(id) {
+  const list = getWatchList().filter(m => m.id !== id)
   localStorage.setItem('watchlist', JSON.stringify(list))
 }
