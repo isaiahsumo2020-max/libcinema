1fea70bbcb7afb454650787e65bf74994594b927
isaiahsumo2020-max <isaiahsumo2020@gmail.com>
2025-12-27 20:35:59 +0000

    Fix WatchList import path

--- netlify/functions/tmdb.js
+++ netlify/functions/tmdb.js
@@ -1,8 +1,16 @@
-// netlify functions 
-export async function handler(event, context) {
+export async function handler(event) {
   const API_KEY = process.env.VITE_TMDB_API_KEY
-  const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+  const BASE_URL = 'https://api.themoviedb.org/3'
+
+  // Example: /tmdb?path=/movie/123&extra=videos
+  const path = event.queryStringParameters.path || '/movie/popular'
+  const page = event.queryStringParameters.page || 1
+  const query = new URLSearchParams(event.queryStringParameters)
+
+  const url = `${BASE_URL}${path}?api_key=${API_KEY}&${query.toString()}`
+  const response = await fetch(url)
   const data = await response.json()
+
   return {
     statusCode: 200,
     body: JSON.stringify(data),
